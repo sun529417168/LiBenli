f0838c22136a787b03a535c5551a195a4632b392
huaer123 <1808816593@qq.com>
2017-04-23 19:58:21 +0800

    我-修改道馆信息

--- app/src/main/java/com/libenli/fragment/MineFragment.java
+++ app/src/main/java/com/libenli/fragment/MineFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.libenli.R;
 import com.libenli.base.BaseFragment;
@@ -22,6 +24,10 @@ import com.libenli.base.BaseFragment;
 public class MineFragment extends BaseFragment {
     private Context context;
 
+    private TextView tv_title;
+    private RelativeLayout rl_back;
+    private TextView tv_exit;
+
     @Override
     protected View setView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         context = inflater.getContext();
@@ -35,7 +41,14 @@ public class MineFragment extends BaseFragment {
 
     @Override
     protected void init(View rootView) {
+        tv_title = (TextView) rootView.findViewById(R.id.tv_title);
+        tv_title.setText(getResources().getString(R.string.mine));
+        rl_back = (RelativeLayout) rootView.findViewById(R.id.rl_back);
+        rl_back.setVisibility(View.GONE);
+        tv_exit = (TextView) rootView.findViewById(R.id.tv_exit);
+        tv_exit.setVisibility(View.VISIBLE);
     }
+
 
 
 }
